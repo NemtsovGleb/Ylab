af44ef1b0be29e2b18745067aa38a0fcf137b173
Nemtsov <glebka.nemtsov1611@gmail.com>
2024-10-19 16:40:54 +1000

    start doing Task2

---
+++ HabitTrackerTask2/src/test/java/PeopleRepository/PeopleRepositoryTest.java
@@ -0,0 +1,15 @@
+package PeopleRepository;
+
+import models.Person;
+import repositories.PeopleRepository;
+
+import java.util.List;
+
+public class PeopleRepositoryTest {
+
+    private PeopleRepository peopleRepository;
+    private List<Person> mockPeopleList;
+
+
+
+}
